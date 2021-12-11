ba425f3dd728a443ae7d42e8655c19985e771d19
hafizudinjoe <brelism94@gmail.com>
2021-12-11 19:10:53 +0800

    add config

---
+++ settings.py
@@ -0,0 +1,15 @@
+CHROME_DRIVER = r"..\drivers\chromedriver.exe"
+
+EDGE_DRIVER = r"..\drivers\msedgedriver.exe"
+
+INIT_MOVE_X: int = -1920
+
+WAIT_QUIT = 3
+
+SELENIUM_URL = "http://selenium.dev"
+GOOGLE_URL = "https://www.google.com"
+YAHOO_URL = "https://www.yahoo.com"
+MICROSOFT_URL = "https://www.microsoft.com"
+BING_URL = "https://www.bing.com"
+DEMO_HOME_URL = "http://demoselenium.newtonicaonline.com/"
+
